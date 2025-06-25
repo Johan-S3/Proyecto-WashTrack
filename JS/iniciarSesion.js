f50ee20b384c3f9a83fa358e7ae57810ace46316
Johan Polo <johan.s310806@gmail.com>
2025-06-25 11:59:05 -0500

    Avance en validacion de campos

--- JS/iniciarSesion.js
+++ JS/iniciarSesion.js
@@ -1,33 +1,62 @@
+// Importo los meotdos que se necesitan.
+import { outFocus, validarCaracteres, validarFormulario, validarNumeros } from "./module.js";
 
-// Obtén referencia al formulario de login
+// Obtengo la referencia del formulario por el ID
 const formLogin = document.getElementById('form-Login');
+// Obtengo la referencia de cedula y contrasena por el name del input
+const cedula = formLogin.querySelector('input[name="documento"]');
+const contrasena = formLogin.querySelector('input[name="contrasenia"]');
+
+// Agrego eventos para permitir solo la entradas de numero y caracteres a los campos.
+cedula.addEventListener('keydown', validarNumeros);
+contrasena.addEventListener('keydown', validarCaracteres);
+
+// Declaro y defino un arreglo con los campos del formualrio.
+const campos = [cedula, contrasena];
+
+// Recorro el arreglo para agregar evento a cada campo; al momento de perder el enfoque se ejecuta el metodo outFocus.
+campos.forEach(campo => {
+  campo.addEventListener("blur", outFocus);
+});
+
+// Al formualrio le agrego el evento submit
 formLogin.addEventListener('submit', async (e) => {
-  e.preventDefault();
+  e.preventDefault(); //Se previene el envio del formulario.
 
-  const cedula = formLogin.querySelector('input[name="documento"]');
-  const contrasena = formLogin.querySelector('input[name="contrasenia"]');
+  // Si el metodo validar formualrio retorna falso entonces retorna y no se hace nada.
+  if (!validarFormulario(e)) return;
 
+  // Si no se ha ingresado información en los campos se muestra alerta.
   if (!cedula.value || !contrasena.value) {
     alert('Por favor completa todos los campos');
     return;
   }
+
+  // Try..catch para poder ver ele error.
   try {
+    // En una variable almaceno la respuesta de hacer fetch a la ruta que me consulta la cedula ingresada-
     const respuesta = await fetch(`http://localhost:8080/proyecto/api/usuarios/${cedula.value}`);
+
+    // Si la respuesta no está ok entonces muestra alerta y retorna.
     if (!respuesta.ok) {
       alert('Usuario no encontrado');
       return;
     }
+
+    // En una variable almaceno la respuesta en JSON.
     const usuario = await respuesta.json();
-    console.log(usuario);
+    console.log(usuario); //Muestro la respuesta
     
+    // Si el valor ingresado en el campo contrasena es igual a la contraseña del usuario obtenido muestra aleta de bienvenida, resetea el formualrio y redirige a la pagina de incio.
     if (usuario.contrasena === contrasena.value) {
         alert(`¡Bienvenido, ${usuario.nombre}!`);
         formLogin.reset();
         window.location.href = 'inicio.html';
-    } else {
+    } else { //Si no muestra alerta
       alert('Contraseña incorrecta');
     }
   } catch(error) {
+    // Muestro mensaje personalizado y el error que se ocacionó.
     alert('Error de red,   inténtalo más tarde.');
     console.error(error);
   }
--- JS/registro.js
+++ JS/registro.js
@@ -1,47 +1,78 @@
+// Importo los meotdos que se necesitan.
+
+import { outFocus, validarCaracteres, validarFormulario, validarLetras, validarNumeros } from "./module.js";
+
+// Obtengo la referencia del formulario por el ID
 const formRegistro = document.getElementById("form-Registro");
 
+// Obtengo la referencia de los inputs del formulario por su name
+const cedula = formRegistro.querySelector('input[name="cedula"]');
+const nombre = formRegistro.querySelector('input[name="nombre"]');
+const correo = formRegistro.querySelector('input[name="correo"]');
+const telefono = formRegistro.querySelector('input[name="telefono"]');
+const rol = formRegistro.querySelector('select[name="rol"]');
+const contrasena = formRegistro.querySelector('input[name="contrasenia"]');
+
+// Agrego eventos para permitir solo la entradas de numeros, letras y caracteres a los campos correspondientemente.
+cedula.addEventListener('keydown', validarNumeros);
+nombre.addEventListener('keydown', validarLetras);
+telefono.addEventListener('keydown', validarNumeros);
+contrasena.addEventListener('keydown', validarCaracteres);
+
+// Declaro y defino un arreglo con los campos del formualrio.
+const campos = [cedula, nombre, correo, telefono, rol, contrasena];
+
+// Recorro el arreglo para agregar evento a cada campo; al momento de perder el enfoque se ejecuta el metodo outFocus.
+campos.forEach(campo => {
+  campo.addEventListener("blur", outFocus);
+});
+
+// Al formualrio le agrego el evento submit
 formRegistro.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault(); //Se previene el envio del formulario.
 
-    const cedula = formRegistro.querySelector('input[name="cedula"]');
-    const nombre = formRegistro.querySelector('input[name="nombre"]');
-    const correo = formRegistro.querySelector('input[name="correo"]');
-    const telefono = formRegistro.querySelector('input[name="telefono"]');
-    const rol = formRegistro.querySelector('select[name="rol"]');
-    const contrasena = formRegistro.querySelector('input[name="contrasenia"]');
+  // Si el metodo validar formualrio retorna falso entonces retorna y no se hace nada.
+  if (!validarFormulario(e)) return;
+  
 
-    const usuario = {
-        cedula: cedula.value,
-        nombre: nombre.value,
-        correo: correo.value,
-        telefono: telefono.value,
-        // rol: rol.value,
-        contrasena: contrasena.value
-    }
+  // Creo un objeto con los valores agregados en el formualrio.
+  const usuario = {
+      cedula: cedula.value,
+      nombre: nombre.value,
+      correo: correo.value,
+      telefono: telefono.value,
+      // rol: rol.value,
+      contrasena: contrasena.value
+  }
 
-    console.log(usuario);
+  console.log(usuario); //Imprimo el objeto por consola
+  
+  // Si alguno de los campos está vacio solicito ingresar todos los campos
+  if (!usuario.cedula || !usuario.nombre || !usuario.correo || !usuario.telefono || !usuario.contrasena) {
+      alert('Por favor completa todos los campos');
+      return;
+  }
+
+  // Try..catch para poder ver ele error.
+  try {
+    // En una variable almaceno la respuesta de hacer fetch a la ruta que me consulta.
+    const respuesta = await fetch('http://localhost:8080/proyecto/api/usuarios', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(usuario)
+    });
     
-    if (!usuario.cedula || !usuario.nombre || !usuario.correo || !usuario.telefono || !usuario.contrasena) {
-        alert('Por favor completa todos los campos');
-        return;
-    }
-
-    try {
-        const respuesta = await fetch('http://localhost:8080/proyecto/api/usuarios', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(usuario)
-        });
-        if (respuesta.ok) {
-        alert('Registro exitoso. Ya puedes iniciar sesión.');
-        formRegistro.reset();
-        window.location.href = 'index.html';
+    // Si la respuesta no está ok entonces muestra alerta y retorna.
+    if (respuesta.ok) {
+      alert('Registro exitoso. Ya puedes iniciar sesión.');
+      formRegistro.reset();
+      window.location.href = 'index.html';
     } else {
-        const errorTexto = await respuesta.text();
-        alert('Error al registrar: ' + errorTexto);
-        }
-    } catch (error) {
+      const errorTexto = await respuesta.text();
+      alert('Error al registrar: ' + errorTexto);
+      }
+  } catch (error) {
     alert('Error de red, inténtalo más tarde.');
     console.error(error);
-    }
+  }
 })
